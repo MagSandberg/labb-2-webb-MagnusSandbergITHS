933049bab457da8022e7dca5c501aee5f49a6a21
MagnusSandbergITHS <magnus.sandberg@iths.se>
2023-02-16 10:40:06 +0100

    Initial push to setup project

---
+++ Labb_2_webb-REST-Api/Labb_2_webb-REST-Api/Shared/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Labb_2_webb_REST_Api.Shared
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
