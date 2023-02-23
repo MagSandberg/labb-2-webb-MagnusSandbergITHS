18df4f4a94638310de1a1c5aa56776628bafeafb
Magnus Sandberg <112380936+MagnusSandbergITHS@users.noreply.github.com>
2023-02-23 10:25:39 +0100

    Delete RESTApi directory

--- RESTApi/RESTApi/Shared/DTOs/OrderDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RESTApi.Shared.DTOs;
-
-public class OrderDto
-{
-    public string CustomerEmail { get; set; } = string.Empty;
-    public List<string> ProductList { get; set; } = new();
-}
