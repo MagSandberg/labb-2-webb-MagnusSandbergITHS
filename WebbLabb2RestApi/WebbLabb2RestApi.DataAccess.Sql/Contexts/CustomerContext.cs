ec4cf262c5d27d33894384078eb8d2e2ebc6e512
MagnusSandbergITHS <magnus.sandberg@iths.se>
2023-02-26 13:52:47 +0100

    .

--- WebbLabb2RestApi/WebbLabb2RestApi.DataAccess.Sql/Contexts/CustomerContext.cs
+++ WebbLabb2RestApi/WebbLabb2RestApi.DataAccess.Sql/Contexts/CustomerContext.cs
@@ -1,6 +1,36 @@
-﻿namespace WebbLabb2RestApi.DataAccess.Sql.Contexts;
+﻿using Microsoft.EntityFrameworkCore;
+using WebbLabb2RestApi.DataAccess.Sql.Models;
 
-public class CustomerContext
+namespace WebbLabb2RestApi.DataAccess.Sql.Contexts;
+
+public class CustomerContext : DbContext
 {
-    
+    public DbSet<CustomerModel> CustomerModel { get; set; } = null!;
+
+    public CustomerContext(DbContextOptions<CustomerContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=CustomerDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CustomerModel>(entity =>
+        {
+            entity.HasKey(e => e.CustomerId).HasName("PK_Customer_Model");
+
+            entity.Property(e => e.CustomerId).ValueGeneratedOnAdd();
+            entity.Property(e => e.FirstName).HasMaxLength(100);
+            entity.Property(e => e.LastName).HasMaxLength(100);
+            entity.Property(e => e.Email).HasMaxLength(255).IsRequired();
+            entity.Property(e => e.CellNumber).HasMaxLength(20);
+            entity.Property(e => e.StreetAddress).HasMaxLength(100);
+            entity.Property(e => e.City).HasMaxLength(100);
+            entity.Property(e => e.ZipCode).HasMaxLength(5);
+
+        });
+    }
 }
--- WebbLabb2RestApi/WebbLabb2RestApi.DataAccess.Sql/Repositories/CustomerRepository.cs
+++ WebbLabb2RestApi/WebbLabb2RestApi.DataAccess.Sql/Repositories/CustomerRepository.cs
@@ -5,31 +5,31 @@ namespace WebbLabb2RestApi.DataAccess.Sql.Repositories;
 
 public class CustomerRepository
 {
-    private CustomerModel ConvertToModel(CustomerDto dto)
-    {
-        return new CustomerModel()
-        {
-            FirstName = dto.FirstName,
-            LastName = dto.LastName,
-            Email = dto.Email,
-            CellNumber = dto.CellNumber,
-            StreetAddress = dto.StreetAddress,
-            City = dto.City,
-            ZipCode = dto.ZipCode
-        };
-    }
+    //private CustomerModel ConvertToModel(CustomerDto dto)
+    //{
+    //    return new CustomerModel()
+    //    {
+    //        FirstName = dto.FirstName,
+    //        LastName = dto.LastName,
+    //        Email = dto.Email,
+    //        CellNumber = dto.CellNumber,
+    //        StreetAddress = dto.StreetAddress,
+    //        City = dto.City,
+    //        ZipCode = dto.ZipCode
+    //    };
+    //}
 
-    private CustomerDto ConvertToDto(CustomerModel dataModel)
-    {
-        return new CustomerDto()
-        {
-            FirstName = dataModel.FirstName,
-            LastName = dataModel.LastName,
-            Email = dataModel.Email,
-            CellNumber = dataModel.CellNumber,
-            StreetAddress = dataModel.StreetAddress,
-            City = dataModel.City,
-            ZipCode = dataModel.ZipCode
-        };
-    }
+    //private CustomerDto ConvertToDto(CustomerModel dataModel)
+    //{
+    //    return new CustomerDto()
+    //    {
+    //        FirstName = dataModel.FirstName,
+    //        LastName = dataModel.LastName,
+    //        Email = dataModel.Email,
+    //        CellNumber = dataModel.CellNumber,
+    //        StreetAddress = dataModel.StreetAddress,
+    //        City = dataModel.City,
+    //        ZipCode = dataModel.ZipCode
+    //    };
+    //}
 }
