0d6d5ed4675dfb57356c0b19c6261266ba13d03e
MagnusSandbergITHS <magnus.sandberg@iths.se>
2023-02-26 20:23:00 +0100

    Försök att få sql databasen att generreras

--- WebbLabb2RestApi/WebbLabb2RestApi.DataAccess.Sql/Contexts/CustomerContext.cs
+++ WebbLabb2RestApi/WebbLabb2RestApi.DataAccess.Sql/Contexts/CustomerContext.cs
@@ -5,16 +5,11 @@ namespace WebbLabb2RestApi.DataAccess.Sql.Contexts;
 
 public class CustomerContext : DbContext
 {
-    public DbSet<CustomerModel> CustomerModel { get; set; } = null!;
-
     public CustomerContext(DbContextOptions<CustomerContext> options) : base(options)
     {
-
     }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=CustomerDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+    public DbSet<CustomerModel> CustomerModel { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- WebbLabb2RestApi/WebbLabb2RestApi/Client/Program.cs
+++ WebbLabb2RestApi/WebbLabb2RestApi/Client/Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
 using WebbLabb2RestApi.Client;
+using WebbLabb2RestApi.DataAccess.Sql.Contexts;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddDbContext<CustomerContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("CustomerDb")
+));
 
 await builder.Build().RunAsync();
