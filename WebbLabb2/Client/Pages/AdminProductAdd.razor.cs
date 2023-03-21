4dbe0bea96bfb5ed85ff2d014347c7d26cfa156e
MagnusSandbergITHS <magnus.sandberg@iths.se>
2023-03-21 15:45:54 +0100

    small fix

--- WebbLabb2/Client/Pages/AdminProductAdd.razor.cs
+++ WebbLabb2/Client/Pages/AdminProductAdd.razor.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+using WebbLabb2.Shared.DTOs;
 
 namespace WebbLabb2.Client.Pages;
 
 public partial class AdminProductAdd : ComponentBase
 {
-    
+    public ProductDto Product { get; set; } = new ();
+    public async Task CreateProduct()
+    {
+        await PublicClient.Client.PostAsJsonAsync("createProduct", Product);
+    }
 }
--- WebbLabb2/Shared/DTOs/CustomerDto.cs
+++ WebbLabb2/Shared/DTOs/CustomerDto.cs
@@ -1,13 +1,28 @@
-﻿namespace WebbLabb2.Shared.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace WebbLabb2.Shared.DTOs;
 
 public class CustomerDto
 {
     public Guid Id { get; set; }
+    [Required]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Name is too long or short.")]
     public string FirstName { get; set; } = string.Empty;
+    [Required]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Name is too long or short.")]
     public string LastName { get; set; } = string.Empty;
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = string.Empty;
+    [Required]
     public string CellNumber { get; set; } = string.Empty;
+    [Required]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Name is too long or short.")]
     public string StreetAddress { get; set; } = string.Empty;
+    [Required]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Name is too long or short.")]
     public string City { get; set; } = string.Empty;
+    [Required]
     public int ZipCode { get; set; }
 }
